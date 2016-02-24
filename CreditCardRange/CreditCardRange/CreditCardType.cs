9c6a06c4d6354ada8a8eaf0cb91d8639f2092c2c
John Colagioia <john@colagioia.net>
2016-02-24 06:31:15 -0500

    Add major American issuers

--- CreditCardRange/CreditCardRange/CreditCardType.cs
+++ CreditCardRange/CreditCardRange/CreditCardType.cs
@@ -1,8 +1,23 @@
+using System.ComponentModel;
+
 namespace CreditCardProcessing
 {
     public enum CreditCardType
     {
+		[Description("Unknown Issuer")]
         Unknown,
+
+		[Description("American Express")]
+		AmEx,
+
+		[Description("Discover Card")]
+		Discover,
+
+		[Description("MasterCard")]
+		MasterCard,
+
+		[Description("Visa")]
+		Visa,
     }
 }
 
