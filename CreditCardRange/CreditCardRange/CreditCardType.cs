592d274dd00ecd9eaefe4393b20dc004d14f2c62
John Colagioia <john@colagioia.net>
2016-02-26 06:41:53 -0500

    Add comments to type enumeration

--- CreditCardRange/CreditCardRange/CreditCardType.cs
+++ CreditCardRange/CreditCardRange/CreditCardType.cs
@@ -1,23 +1,43 @@
-using System.ComponentModel;
-
+/// <summary>
+/// Credit card type.
+/// </summary>
 namespace CreditCardProcessing
 {
+	using System.ComponentModel;
+
+	/// <summary>
+	/// Credit card type.
+	/// </summary>
     public enum CreditCardType
     {
+		/// <summary>
+		/// Unknown issuers.
+		/// </summary>
 		[Description("Unknown Issuer")]
         Unknown,
 
+		/// <summary>
+		/// American Express.
+		/// </summary>
 		[Description("American Express")]
 		AmEx,
 
+		/// <summary>
+		/// Discover Card.
+		/// </summary>
 		[Description("Discover Card")]
 		Discover,
 
+		/// <summary>
+		/// Master Card.
+		/// </summary>
 		[Description("MasterCard")]
 		MasterCard,
 
+		/// <summary>
+		/// Visa.
+		/// </summary>
 		[Description("Visa")]
 		Visa,
     }
 }
-
