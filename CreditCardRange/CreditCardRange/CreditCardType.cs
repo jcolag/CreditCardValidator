16c7126fa7bc85a36079454e7f40ae4b92cc9c72
John Colagioia <john@colagioia.net>
2016-03-06 09:42:40 -0500

    Add enumeration values for other card types

--- CreditCardRange/CreditCardRange/CreditCardType.cs
+++ CreditCardRange/CreditCardRange/CreditCardType.cs
@@ -45,5 +45,107 @@ namespace CreditCardProcessing
         /// </summary>
         [Description("Visa")]
         Visa,
+
+        /// <summary>
+        /// Bankcard cards.
+        /// </summary>
+        [Description("Bankcard")]
+        Bankcard,
+
+        /// <summary>
+        /// China Union Pay cards.
+        /// </summary>
+        [Description("China Union Pay")]
+        ChinaUnionPay,
+
+        /// <summary>
+        /// Diners Club Carte Blanche cards.
+        /// </summary>
+        [Description("Diners Club Carte Blanche")]
+        DCCarteBlanche,
+
+        /// <summary>
+        /// Diners Club enRoute cards.
+        /// </summary>
+        [Description("Diners Club enRoute")]
+        DCEnRoute,
+
+        /// <summary>
+        /// Diners Club International cards.
+        /// </summary>
+        [Description("Diners Club International")]
+        DCInternational,
+
+        /// <summary>
+        /// Diners Club US and Canada cards.
+        /// </summary>
+        [Description("Diners Club US and Canada")]
+        DCUSCan,
+
+        /// <summary>
+        /// InterPayment cards.
+        /// </summary>
+        [Description("InterPayment")]
+        InterPayment,
+
+        /// <summary>
+        /// InstaPayment cards.
+        /// </summary>
+        [Description("InstaPayment")]
+        InstaPayment,
+
+        /// <summary>
+        /// JCB cards.
+        /// </summary>
+        [Description("JCB")]
+        JCB,
+
+        /// <summary>
+        /// Laser cards.
+        /// </summary>
+        [Description("Laser")]
+        Laser,
+
+        /// <summary>
+        /// Maestro cards.
+        /// </summary>
+        [Description("Maestro")]
+        Maestro,
+
+        /// <summary>
+        /// Dankort cards.
+        /// </summary>
+        [Description("Dankort")]
+        Dankort,
+
+        /// <summary>
+        /// Solo cards.
+        /// </summary>
+        [Description("Solo")]
+        Solo,
+
+        /// <summary>
+        /// Switch cards.
+        /// </summary>
+        [Description("Switch")]
+        Switch,
+
+        /// <summary>
+        /// UATP cards.
+        /// </summary>
+        [Description("UATP")]
+        UATP,
+
+        /// <summary>
+        /// Verve cards.
+        /// </summary>
+        [Description("Verve")]
+        Verve,
+
+        /// <summary>
+        /// Cardguard cards.
+        /// </summary>
+        [Description("Cardguard EAD BG ILS")]
+        Cardguard,
     }
 }
